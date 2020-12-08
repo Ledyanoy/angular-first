e9fcc1aee0d950fc5e10b20adf1abfc2a3667787
Коренев Александр <akorenev@local.htc-cs.com>
2020-12-08 17:23:42 +0400

    create angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'first';
+  number = 4345354;
+  reactLogo = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+
+  constructor() {
+    setTimeout(()=> {
+      console.log('change image', this.reactLogo)
+      this.reactLogo = 'https://angular.io/assets/images/logos/angular/angular.svg'
+    }, 5000)
+  }
 }
