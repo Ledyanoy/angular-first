053233b08f0c114e03bc0bf4801f8962e0ca7bd3
Коренев Александр <akorenev@local.htc-cs.com>
2020-12-11 15:54:30 +0400

    pipe and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,36 @@ export class AppComponent {
   reactLogo = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg';
   inputValue = '';
   secondTitle = 'Initial';
-  backGroundToggle = false;
+  backGroundToggle = true;
+  paragraphCase = 2;
+  newArray: Array<string> = [
+    'vot first elem',
+    'vot second elem',
+    'vot third elem',
+    'vot fourth elem',
+    'vot fifth elem',
+  ];
+  arrayWithObjects = [
+    {
+      name: 'Alexander',
+      post: 'Whats up bitches!',
+      comments: [
+        {name: 'Mike', comment: 'Bruh))'},
+        {name: 'Rust88', comment: 'He'},
+        {name: 'Kate', comment: 'Hi'},
+      ]
+    },
+    {
+      name: 'Tobias',
+      post: 'Hi im here now',
+      comments: [
+        {name: 'Mike', comment: 'Great'},
+        {name: 'Rust88', comment: 'Wow'},
+        {name: 'Kate', comment: 'Hi'},
+      ]
+    }
+  ];
+
 
   constructor() {
     setTimeout(() => {
@@ -22,7 +51,7 @@ export class AppComponent {
   }
 
   onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value;
+    this.inputValue = (<HTMLInputElement> event.target).value;
   }
 
   onBlur(str: string) {
